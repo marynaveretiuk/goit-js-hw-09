4de7e6532082dcad60041c133ca611c3b1f88d15
Maryna <veretiuk@gmail.com>
2026-01-16 08:31:33 -0500

    final

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,53 @@
-console.log('Form');
+const formData = {
+    email: "",
+    message: "",
+}
+
+const form = document.querySelector(".feedback-form");
+
+
+const savedData = localStorage.getItem("feedback-form-state");
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+/* ---------- INPUT ---------- */
+form.addEventListener("input", (ev) => {
+  const field = ev.target;
+  const fieldName = field.name;
+  const fieldValue = field.value;
+
+  formData[fieldName] = fieldValue.trim();
+
+
+  localStorage.setItem(
+    "feedback-form-state",
+    JSON.stringify(formData)
+  );
+});
+
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem("feedback-form-state");
+
+  formData.email = "";
+  formData.message = "";
+
+  form.reset();
+});
